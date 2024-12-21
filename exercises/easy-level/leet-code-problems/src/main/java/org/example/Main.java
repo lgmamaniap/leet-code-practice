5eb712a166f31e65ad0bd6bb28e6fbb97d876070
gustavo <lgmamaniap@gmail.com>
2024-12-21 01:54:44 -0400

    feat: :sparkles: find nearest point in java - solution

---
+++ exercises/easy-level/leet-code-problems/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.solutions.NearestValidPoint;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Nearest point
+        NearestValidPoint nVP = new NearestValidPoint();
+        nVP.testNearestValidPoint();
+    }
+}
