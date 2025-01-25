69faafcfb8be178fc7dd9021a479201521ba0a4d
gustavo <lgmamaniap@gmail.com>
2025-01-25 00:11:01 -0400

    feat: :sparkles: sum of unique elements (improve presentation) - c++ - solution

--- exercises/easy-level/sumOfUniqueElements.cpp
+++ exercises/easy-level/sumOfUniqueElements.cpp
@@ -8,18 +8,11 @@ int sumOfUnique(vector<int> &nums)
 {
   int acc[105] = {};
   for (int i = 0; i < nums.size(); i++)
-  {
     acc[nums[i]]++;
-  }
 
   int sum = 0;
   for (int i = 1; i <= 100; i++)
-  {
-    if (acc[i] == 1)
-    {
-      sum += i;
-    }
-  }
+    if (acc[i] == 1) sum += i;
 
   return sum;
 }
