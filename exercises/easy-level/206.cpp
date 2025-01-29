312a223c93e29988efedacf610b442912ad80981
gustavo <lgmamaniap@gmail.com>
2025-01-28 23:40:07 -0400

    feat: :sparkles: reverse linked list - c++ - solution

---
+++ exercises/easy-level/206.cpp
@@ -0,0 +1,58 @@
+// 206. Reverse Linked List
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *reverseList(ListNode *head)
+{
+  ListNode *prev = nullptr; // Nuevo último nodo (inicialmente nullptr)
+  ListNode *current = head; // Puntero para recorrer la lista
+
+  while (current != nullptr)
+  {
+    ListNode *nextNode = current->next; // Guardamos el siguiente nodo
+    current->next = prev;               // Invertimos el puntero
+
+    // Avanzamos los punteros
+    prev = current;
+    current = nextNode;
+  }
+
+  return prev; // `prev` es el nuevo head de la lista invertida
+}
+
+int main()
+{
+  return 0;
+}
+
+/*
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+
+Example 3:
+Input: head = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+*/
